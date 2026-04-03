cd2ae716986420f5036ca82fbc4e8ce170252af0
faiyaZzz75 <faiyaztahmid@iut-dhaka.edu>
2026-04-04 01:30:07 +0600

    Create HumanWorker.java

---
+++ isp/HumanWorker.java
@@ -0,0 +1,12 @@
+package isp;
+
+public class HumanWorker implements Workable, Eatable, Sleepable {
+    @Override
+    public void work()  { System.out.println("Human is working."); }
+
+    @Override
+    public void eat()   { System.out.println("Human is eating."); }
+
+    @Override
+    public void sleep() { System.out.println("Human is sleeping."); }
+}
